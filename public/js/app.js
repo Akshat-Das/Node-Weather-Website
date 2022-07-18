ff708f553e13fcf5a3d30bd6a98a59b3cbabee32
Akshat Das <akshatdas4@gmail.com>
2022-07-18 12:06:04 +0530

    heroku changes

--- public/js/app.js
+++ public/js/app.js
@@ -7,16 +7,14 @@ weatherForm.addEventListener("submit", (e) => {
   e.preventDefault();
   message1.textContent = "Loading...";
   message2.textContent = "";
-  fetch("http://localhost:3000/weather?address=" + search.value).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          message1.textContent = data.error;
-        } else {
-          message1.textContent = data.location;
-          message2.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch("/weather?address=" + search.value).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        message1.textContent = data.error;
+      } else {
+        message1.textContent = data.location;
+        message2.textContent = data.forecast;
+      }
+    });
+  });
 });
