f1bf8a2b4c92d5aba3f107d81f1c2b427a3123cc
Akshat Das <akshatdas4@gmail.com>
2022-07-18 12:20:14 +0530

    Added ectra forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,31 @@
-const request = require('request')
+const request = require("request");
 
-const forecast = (lat,long,callback) =>{
-    const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+ lat+ '&lon=' + long + '&appid=88c115331c5a695f2f907989e5f80181&units=metric'
-    request({url,json:true},(error,{body}) => {
-        if(error){
-            callback('Unable to connect to weather service!')
-        }else if(body.message){
-            callback(body.message)
-        }else{
-            callback(undefined,"It is currently " + body.main.temp + " degrees outside. The humidity is " + body.main.humidity)
-        }
-    })
-}
+const forecast = (lat, long, callback) => {
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&appid=88c115331c5a695f2f907989e5f80181&units=metric";
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!");
+    } else if (body.message) {
+      callback(body.message);
+    } else {
+      callback(
+        undefined,
+        "It is currently " +
+          body.main.temp +
+          " degrees outside. The humidity is " +
+          body.main.humidity +
+          ". The maximum temperature today will be " +
+          body.main.temp_max +
+          " and the minimum temperature today will be " +
+          body.main.temp_min
+      );
+    }
+  });
+};
 
-module.exports = forecast
+module.exports = forecast;
